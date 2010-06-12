35f458ac4e263b805b0d90fa84e6f78df0f21cf5
Helen.Fornazier <Helen.Fornazier@2d26c7e0-daf7-0f5f-5b55-53661da1b250>
2010-06-12 15:16:39 +0000

    first commit again

---
+++ projeto436/work/org/apache/jsp/barraHorizontal_jsp.java
@@ -0,0 +1,81 @@
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class barraHorizontal_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static java.util.List _jspx_dependants;
+
+  public Object getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    JspFactory _jspxFactory = null;
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+
+    try {
+      _jspxFactory = JspFactory.getDefaultFactory();
+      response.setContentType("text/html");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+
+      out.write("\t<!-- end #logo -->\r\n");
+      out.write("\t<div id=\"header\">\r\n");
+      out.write("\t\t<div id=\"menu\">\r\n");
+      out.write("\t\t\t<ul>\r\n");
+      out.write("\t\t\t\t<li><a href=\"CarregaInicio\">Inicio</a></li>\r\n");
+      out.write("\t\t\t</ul>\r\n");
+      out.write("\t\t</div>\r\n");
+      out.write("\t\t<!-- end #menu -->\r\n");
+      out.write("\t\t<div id=\"search\">\r\n");
+      out.write("\t\t\t<form action=\"EfetuaLogin\" method=\"get\">\r\n");
+      out.write("\t\t\t<table>\r\n");
+      out.write("\t\t\t<tr>\r\n");
+      out.write("\t\t\t\t<td><font size=\"2\">usuario</font></td>\r\n");
+      out.write("\t\t\t\t<td><input type=\"text\" name=\"usuario\" size=\"11\"></td>\r\n");
+      out.write("\t\t\t\t<td><font size=\"2\">senha</font></td>\r\n");
+      out.write("\t\t\t\t<td><input type=\"password\" name=\"password\" size=\"11\"></td>\r\n");
+      out.write("\t\t\t\t<td><input type=\"submit\" value=\"      Entrar      \" name=\"botaoLogin\"></td>\r\n");
+      out.write("\t\t\t</tr>\r\n");
+      out.write("\t\t\t</table>\r\n");
+      out.write("\t\t\t</form>\r\n");
+      out.write("\t\t</div>\r\n");
+      out.write("\t\t<!-- end #search -->\r\n");
+      out.write("\t</div>\r\n");
+      out.write("\t<!-- end #header -->\r\n");
+      out.write("</div>\r\n");
+      out.write("<!-- end #header-wrapper -->\r\n");
+      out.write("<div id=\"page\">\r\n");
+      out.write("\t<div id=\"content\">");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+      }
+    } finally {
+      if (_jspxFactory != null) _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
---
+++ projeto436/work/org/apache/jsp/loginSucesso_jsp.java
@@ -0,0 +1,80 @@
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class loginSucesso_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static java.util.List _jspx_dependants;
+
+  public Object getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    JspFactory _jspxFactory = null;
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+
+    try {
+      _jspxFactory = JspFactory.getDefaultFactory();
+      response.setContentType("text/html");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+
+      org.apache.jasper.runtime.JspRuntimeLibrary.include(request, response, "logo.jsp", out, false);
+      out.write('\r');
+      out.write('\n');
+      org.apache.jasper.runtime.JspRuntimeLibrary.include(request, response, "barraHorizontal.jsp", out, false);
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("     \r\n");
+      out.write("      <!--linhas entre a barra de titulo acima e a tabela abaixo-->       \r\n");
+      out.write("       <p>&nbsp;</p>\r\n");
+      out.write("      <p>&nbsp;</p>\r\n");
+      out.write("      <p>&nbsp;</p>\r\n");
+      out.write("      <p>&nbsp;</p>\r\n");
+      out.write("\r\n");
+      out.write("     \t<p style=\"text-align: center\"><font color=\"#008000\" size=\"5\"><b>Login do \r\n");
+      out.write("        usuario realizado com sucesso!!!</b></font></p>\r\n");
+      out.write("     \r\n");
+      out.write("\r\n");
+      out.write("      <!--linhas em branco-->       \r\n");
+      out.write("      <p>&nbsp;</p>\r\n");
+      out.write("      <p>&nbsp;</p>\r\n");
+      out.write("      <p>&nbsp;</p>\r\n");
+      out.write("      <p>&nbsp;</p>\r\n");
+      out.write("\r\n");
+      org.apache.jasper.runtime.JspRuntimeLibrary.include(request, response, "barraLateral.jsp", out, false);
+      out.write('\r');
+      out.write('\n');
+      org.apache.jasper.runtime.JspRuntimeLibrary.include(request, response, "rodape.jsp", out, false);
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+      }
+    } finally {
+      if (_jspxFactory != null) _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
